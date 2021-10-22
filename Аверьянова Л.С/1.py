77fad9f3c9ef0462857ec60ea4d29c096f706a72
avery <lumusnoks71@gmail.com>
2021-10-22 17:31:05 +0300

    [1]

--- Аверьянова Л.С/1.py
+++ Аверьянова Л.С/1.py
@@ -1,4 +1,4 @@
 import numpty as np
 import pandas as pd
 
-data1 = pd.read_csv('Kaggle_Marketing_campain/Data/marketing_campain.csv')
+data = pd.read_csv('Kaggle_Marketing_campain/Data/marketing_campain.csv')
