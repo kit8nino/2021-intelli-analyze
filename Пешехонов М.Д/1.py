d88a0ebe65c27b66b38c34641e453a86875cdcfe
Excome <56866949+Excome@users.noreply.github.com>
2021-11-28 23:55:16 +0300

    [1] Оценка взаимосвязей, корреляция

--- Пешехонов М.Д/1.py
+++ Пешехонов М.Д/1.py
@@ -1,4 +1,26 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('Kaggle_Marketing_compain/Data/marketing_compain.csv')
+
+# Пешехонов М. Д. гр.ИС-27
+# Вариант 'e'
+# Проанализировать и выдать статистические данные по связи между:
+#   e) благородностью и номером первой буквы имени (A -1, B -2 и т.д)
+
+def execute_task(data):
+    i = 0
+    for x in data['Name']:
+        data['Name'][i] = ord(x[0].lower()) - 96
+        i += 1
+    return np.corrcoef(data['Name'].to_list(), data['Nobility'].to_list())[0, 1]
+
+
+if __name__ == '__main__':
+    pd.options.mode.chained_assignment = None
+    df = pd.read_csv('../_lab-1/character-deaths.csv')
+    corr = execute_task(df[['Name', 'Nobility']])
+    print(f"Связь имеется и её велечина = {corr}")
+    if corr > 0:
+        print("Прямая связь между благородностью и номером первой буквы имени")
+    else:
+        print("Обратная связь между благородностью и номером первой буквы имени")
