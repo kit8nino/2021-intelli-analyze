53970b87ec85d940d527dea03a4ff99653ab2840
KupriyanovaTatyana <53702010+KupriyanovaTatyana@users.noreply.github.com>
2021-11-29 11:22:52 +0300

    [1] Оценка взаимосвязей, корреляция
    [1] Оценка взаимосвязей, корреляция

--- Куприянова Т.В/[1] Оценка взаимосвязей, корреляция.py
+++ Куприянова Т.В/[1] Оценка взаимосвязей, корреляция.py
@@ -1,10 +1,22 @@
 import pandas as pd
+import numpy as np
 dt=pd.read_csv('D:/work/2021-intelli-analyze/_lab-1/character-deaths.csv')
 
 # Вступление
 print("\nАнализ выполнила студентка группы ИС-27 Куприянова Татьяна")
 print("Вариант задания - 11. Дом Грейджоев - House Greyjoy")
 
+# Анализ связей
+print("\nАнализ связей между домом и разницей в главах")
+print("в которой появился и в которой умер")
+dt = dt[['Allegiances', 'Death Chapter', 'Book Intro Chapter']]
+dt = dt.drop(np.where(np.isnan(dt['Death Chapter']))[0]).fillna(0)
+dt['Chapter diff'] = dt['Death Chapter'] - dt['Book Intro Chapter']
+dt = dt.drop(['Death Chapter', 'Book Intro Chapter'], axis=1).groupby('Allegiances').mean()
+dt = (dt - dt.min()) / (dt.max() - dt.min())
+print(str(dt))
+
+# Общая статистика
 print("\nОбщая статистика смертей в каждом доме")
 dt['Allegiances'].value_counts(normalize=True)
 print("\nИсходя из выведенной информации Дом Грейджоев находится на десятом месте")
@@ -33,12 +45,31 @@ print("в 297 и 298 годах никто не умер")
 print("в 299 году умерло 11 человек, в 300 году умело 3 человека")
 print("Итого умерло 14 человек")
 
+# Nobility - Благородство
+print("\nОбщая статистика в каком доме и сколько было благородных людей")
+dt.groupby('Allegiances').Nobility.value_counts()
+print("\nИсходя из выведенной информации в Доме Грейджоев")
+print("было 19 благородных женщин")
+print("и 5 благородных мужчин")
+
 # Сортировка по алфавиту
 print("\nОбщая статистика, на каком месте каждый дом при алфавитной сортировки")
 dt['Allegiances'].value_counts().sort_index(ascending=True)
 print("\nИсходя из выведенной информации Дом Грейджоев")
 print("на 6 месте в алфавитном порядке")
 
+# Общая Корреляция
+print("\nОбщая статистика с корреляцией")
+dt.corr()
+
+# Корреляция по столбцам
+print("\nКорреляция по году смерти и книге смерти")
+dt['Death Year'].corr(dt['Book of Death'])
+
+print("\nКорреляция по полу и главе вступления в книге")
+dt['Gender'].corr(dt['Book Intro Chapter'])
+
+# Вывод
 print("\nТаким образом, был проведен анализ данных из таблицы. Можно прийти к следующему выводу:")
 print("Из 24 человек умерло 14, а значит в живых осталось лишь 10 человек.")
 print("Количество людей из Дома Грейджоев сократилось примерно в 1,7 раз")
